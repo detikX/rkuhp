744e56242a837d120ede8e6f26b7d14383a70cf7
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-07-21 16:37:19 +0700

    update

--- js/app.js
+++ js/app.js
@@ -270,3 +270,136 @@ btns.forEach((btn) => {
 //     header.classList.remove("sticky");
 //   }
 // }
+
+$.ajax({
+  url: "js/book.json",
+  type: "GET",
+  success: (response) => {
+    // console.log(response);
+    var a;
+    for (a = 0; a < response.length; a++) {
+      var image = response[a].image;
+      var id = response[a].id;
+      var judul = response[a].judul;
+      var modal = response[a].modal;
+      var color = response[a].coloring;
+      var benefit = response[a].content;
+      var h = "";
+      benefit.map((a, i) => {
+        h += `<li><b>${a.pasal}:</b> ${a.text}</li>`;
+      });
+
+      // $(".dedi").append(`
+      //   <h2>${judul}</h2>
+      //   <ul>${h}</ul>
+      //   <img src="${image}" style="width:100%"/>
+      //   <div
+      //     class="btn-modal books"
+      //     data-toggle="modal"
+      //     data-id="${id}"
+      //     data-target="#modalDetail"
+      //   >
+      //     Info Lainnya
+      //   </div>
+      // `);
+      var test = id == 5 ? "hidden" : "";
+      var last = id == 5 ? "" : "hidden";
+      $(".flip-book").append(`
+        <div class="flip" id="p${id}">
+          <div class="back">
+            <h2 class="rkuhp ${last}">Draf RKUHP</h2>
+            <img src="${image}" class="${test}" alt="" />
+            <div
+              class="btn-modal books ${test}"
+              data-toggle="modal"
+              data-id="${id}"
+              data-target="#modalDetail"
+            >
+              Info Lainnya
+            </div>
+            <label for="c${id}" class="back-btn"><i class="fa fa-angle-double-left" aria-hidden="true"></i><div>Kembali</div></label>
+          </div>
+          <div class="front ${color}">
+            <div class="front__title ${color}">
+              <h2>${judul}</h2>
+            </div>
+
+            <ul>
+              ${h}
+            </ul>
+
+            <label for="c${id}" class="next-btn"><div>Lanjut</div><i class="fa fa-angle-double-right" aria-hidden="true"></i></label>
+          </div>
+        </div>
+    `);
+    }
+    $("#modalDetail .modal-body").html(
+      "<p class='font-stdbig'>Pasal ini tidak memiliki kekuatan hukum mengikat. Mahkamah Konstitusi melalui putusan Nomor 013-022/puu-iv/2006 pernah membatalkan pasal ini lantaran dianggap merupakan warisan masa kolonial. Berpotensi menjadi pasal karet dan mengekang kebebasan berpendapat.</p>"
+    );
+    $(".books[data-target='#modalDetail']").on("click", function (i, x) {
+      var dataID = $(this).attr("data-id");
+      console.log(dataID);
+      console.log(response[dataID].modal);
+      $("#modalDetail").modal("show");
+
+      $("#modalDetail .modal-content").html(`
+            <div class="modal-body">
+                <p class="font-stdbig">${response[dataID].modal}</p>
+            </div>
+        `);
+    });
+
+    // var h = "";
+    // benefit.map((a, i) => {
+    //   h += `<li><b>${a.pasal}</b>${a.text}</li>`;
+    // });
+
+    /*$(".flip-book").append(`
+    <div class="flip" id="p1">
+      <div class="back">
+        <img src="${image}" alt="" />
+        <label for="c${id}" class="back-btn">Before</label>
+      </div>
+      <div class="front">
+        <div class="front__title">
+          <h2>${judul}</h2>
+        </div>
+
+        <ul>
+          <li>
+            <b>Pasal 218:</b> Setiap orang yang di muka umum
+            menyerang kehormatan atau harkat dan martabat diri
+            Presiden atau Wakil Presiden dipidana dengan pidana
+            penjara paling lama 3 tahun 6 bulan atau pidana denda
+            paling banyak kategori IV.
+          </li>
+          <li>
+            <b>Pasal 219:</b> Setiap orang yang menyiarkan,
+            mempertunjukkan, atau menempelkan tulisan atau gambar
+            sehingga terlihat oleh umum, memperdengarkan rekaman
+            sehingga terdengar oleh umum, atau menyebarluaskan
+            dengan sarana teknologi informasi yang berisi
+            penyerangan kehormatan atau harkat dan martabat
+            terhadap Presiden atau Wakil Presiden dengan maksud
+            agar isinya diketahui atau lebih diketahui umum
+            dipidana dengan pidana penjara paling lama (empat)
+            tahun 6 (enam) bulan atau pidana denda paling banyak
+            kategori IV.
+          </li>
+        </ul>
+
+        <label for="c1" class="next-btn">NEXT</label>
+      </div>
+    </div>
+    `);*/
+  },
+});
+
+$(".chevron,.click-scroll,.ontop__scroll").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#firstletter").offset().top - 20,
+    },
+    500
+  );
+});
